5a5a62eb18f18702d35987a81f600fe9fceec184
akio0911 <akio0911@gmail.com>
2020-12-27 19:00:39 +0900

    feedback

--- kadai10/TableViewController.swift
+++ kadai10/TableViewController.swift
@@ -12,23 +12,22 @@ final class TableViewController: UITableViewController {
     private let prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 47
+        prefectures.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        var identifier = ""
-        
-        if indexPath.row % 3 == 0 {
+        let identifier: String
+        switch indexPath.row % 3 {
+        case 0:
             identifier = "Cell1"
-        } else if indexPath.row % 3 == 1 {
+        case 1:
             identifier = "Cell2"
-        } else {
+        default:
             identifier = "Cell3"
         }
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifier)! as UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         
         cell.textLabel?.text = prefectures[indexPath.row]
         cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です。"
